b292a6fe7b4b0fe2740e8befbb476c1325e2ab42
Ralf Anton Beier <ralf_beier@me.com>
2018-05-26 17:57:10 +0200

    Added the naked pairs elimination code.

--- Projects/1_Sudoku/solution.py
+++ Projects/1_Sudoku/solution.py
@@ -47,8 +47,16 @@ def naked_twins(values):
     Pseudocode for this algorithm on github:
     https://github.com/udacity/artificial-intelligence/blob/master/Projects/1_Sudoku/pseudocode.md
     """
-    # TODO: Implement this function!
-    raise NotImplementedError
+    for box in peers.keys():
+        digits = values[box]
+        for unit in units[box]:
+            foo = [b for b in unit if digits == values[b] and len(digits) == 2]
+            if len(foo) == 2:
+                for digit in digits:
+                    for peer in unit:
+                        if peer not in foo and digit in values[peer]:
+                            values[peer] = values[peer].replace(digit, '')
+    return values
 
 
 def eliminate(values):
@@ -106,11 +114,9 @@ def reduce_puzzle(values):
         # Check how many boxes have a determined value
         solved_values_before = len([box for box in values.keys() if len(values[box]) == 1])
 
-        # Your code here: Use the Eliminate Strategy
         eliminate(values)
-
-        # Your code here: Use the Only Choice Strategy
         only_choice(values)
+        naked_twins(values)
 
         # Check how many boxes have a determined value, to compare
         solved_values_after = len([box for box in values.keys() if len(values[box]) == 1])
